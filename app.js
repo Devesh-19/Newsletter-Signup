a6aca4d90e5790a64491c11fbbf3e061e70bf758
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-26 18:29:45 +0530

    Started to GET the html and css files on local server

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+{
+    res.sendFile(`${__dirname}/signup.html`);
+})
+
 app.listen(3000, () =>
 {
     console.log("Server is running at port 3000");
