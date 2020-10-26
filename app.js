f7f6cac3af0ab0ca5d0325fd00c414a6d2d036ff
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-27 00:37:04 +0530

    Logging user input from the form

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>
 {
     res.sendFile(`${__dirname}/signup.html`);
+})
+
+app.post("/", (req,res) =>
+{
+    const firstName = req.body.fname
+    const lastName = req.body.lname
+    const email = req.body.email
+    console.log(firstName, lastName, email);
+    res.send("Thank you");
 })
 
 app.listen(3000, () =>
