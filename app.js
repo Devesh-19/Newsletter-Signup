da2b895d2f910c93f03ddc812d59ba193033bef5
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-27 21:48:29 +0530

    Prepping to deploy on heroku

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.post("/failure", (req, res) =>
     res.redirect("/");
 })
 
-app.listen(3000, () =>
+app.listen(process.env.PORT || 3000, () =>
 {
     console.log("Server is running at port 3000");
 });
