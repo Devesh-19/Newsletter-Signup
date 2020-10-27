b0277bb7dff0a6072d52b11c4c90a363cdce81c6
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-27 14:27:35 +0530

    Added the functionality to add the user as a contact and a subscriber using mailchimp API

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
+const { json } = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,8 +20,41 @@ app.post("/", (req,res) =>
     const firstName = req.body.fname
     const lastName = req.body.lname
     const email = req.body.email
-    console.log(firstName, lastName, email);
-    res.send("Thank you");
+    
+    const data = {
+        members: [
+            {
+                email_address : email,
+                status : "subscribed",
+                merge_fields : {
+                    FNAME : firstName,
+                    LNAME : lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const listID = process.env.LIST_ID;
+    const apiKey = process.env.API_KEY;
+
+    const url = `https://us2.api.mailchimp.com/3.0/lists/${listID}`;
+
+    const options = {
+        method : "POST",
+        auth : `devesh1:${apiKey}`
+    }
+    
+    const request = https.request(url, options, response =>
+        {
+            response.on("data", d =>
+            {
+                console.log(JSON.parse(d));
+            })
+        })
+    request.write(jsonData);
+    request.end();
 })
 
 app.listen(3000, () =>
