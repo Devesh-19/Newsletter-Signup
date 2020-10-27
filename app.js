00e3c62b313164a11d3706691a16ad0bb7d6e4c4
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-27 15:47:29 +0530

    Added the success and failure pages

--- app.js
+++ app.js
@@ -48,6 +48,15 @@ app.post("/", (req,res) =>
     
     const request = https.request(url, options, response =>
         {
+            if (response.statusCode === 200)
+            {
+                res.sendFile(`${__dirname}/success.html`);
+            }
+            else
+            {
+                res.sendFile(`${__dirname}/failure.html`);
+            }
+
             response.on("data", d =>
             {
                 console.log(JSON.parse(d));
@@ -55,6 +64,9 @@ app.post("/", (req,res) =>
         })
     request.write(jsonData);
     request.end();
+
+    console.log(res);
+
 })
 
 app.listen(3000, () =>
